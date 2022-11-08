179d611dbd60b9e6c2bfc039d58f03870a2550bf
Zaidibeth Ramos <38891128+zergcore@users.noreply.github.com>
2022-11-08 15:10:43 -0400

    Product Styles

---
+++ src/components/Product.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+function Product({product}) {
+  return (
+    <div className="Products-item">
+        <img src={product.image} alt={product.title} />
+        <div className="Product-item-info">
+            <h2>
+                {product.title}
+                <span>
+                    $
+                    {' '}
+                    {product.price}</span>
+            </h2>
+            <p>{product.description}</p>
+            <button type='button'>Comprar</button>
+        </div>
+    </div>
+  )
+}
+
+export default Product
